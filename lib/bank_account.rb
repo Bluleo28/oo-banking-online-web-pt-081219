62ba8bac7b83d8b6ed1aa98cf95c84e803cff319
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-30 05:14:16 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,6 +16,7 @@ class BankAccount
   
   def display_balance 
     "Your balance is $#{@balance}."
+
   end 
   def valid? 
     @status == 'open' && @balance > 0 
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,15 @@
+require "pry"
+
 class Transfer
   
-end
+  attr_accessor :receiver, :status 
+  attr_reader :sender 
+  
+  def initialize(sender)
+    @sender = sender 
+    @receiver = receiver
+    @status = 'pending'
+    
+  end 
+end 
+    
